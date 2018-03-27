476b318ae5b2dc140468bb404257bd5d7c9c1781
Will <will.umeh.cd2@solutions.co.uk>
2018-03-27 17:00:37 +0100

    asdas

--- src/dsl/validates.js
+++ src/dsl/validates.js
@@ -1,5 +1,5 @@
 export default function validates(target, name, descriptor) {
-  const attribute_name = name[0] === '_' ? name.substring(1, name.length) : name
+  const attribute_name = name[0] === `_` ? name.substring(1, name.length) : name
   const { on, if: ifCallback, ...validators } = descriptor.initializer()
   const validations = Object.entries(validators).map(([validator, options]) => {
     if (options === true) options = {}
