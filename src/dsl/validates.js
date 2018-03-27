a1961c1747594b09e5cbeb800b2f36e8f5923a80
smosix <shane@cd2solutions.co.uk>
2018-03-27 15:35:52 +0100

    asd

--- src/dsl/validates.js
+++ src/dsl/validates.js
@@ -1,11 +1,12 @@
 export default function validates(target, name, descriptor) {
+  const attribute_name = name[0] === '_' ? name.substring(1, name.length) : name
   const { on, if: ifCallback, ...validators } = descriptor.initializer()
   const validations = Object.entries(validators).map(([validator, options]) => {
     if (options === true) options = {}
-    options.attribute = name
+    options.attribute = attribute_name
     return { validator, on, if: ifCallback, options }
   })
-  target.constructor._validators[name] = validations
+  target.constructor._validators[attribute_name] = validations
 
   descriptor.configurable = true
   delete descriptor.value
