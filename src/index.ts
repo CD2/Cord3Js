2874cf977e485d8af09b0c13439c562a425ea919
Henry <hexmodr@gmail.com>
2018-11-11 15:18:08 +0000

    asd

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,4 @@ export { Model, Collection }
 export { default as FileManager } from "./FileManager"
 export { default as Store } from "./Store"
 
-export { default as hasOne } from "./dsl/hasOne"
-export { default as hasMany } from "./dsl/hasMany"
 export { createModel } from "./model/Model"
