6796bb3d0ec97f88365fc46386ebd9375e1c26d7
Henry <hexmodr@gmail.com>
2018-11-11 12:27:23 +0000

    asd

--- src/dsl/mountUploader.ts
+++
@@ -1,8 +0,0 @@
-import FileManager from "../FileManager"
-
-export default function mountUploader(target, name, descriptor) {
-  if (!(target instanceof Function)) target = target.constructor
-  const options = descriptor.initializer ? descriptor.initializer() : {}
-  FileManager.install(target, name, options)
-  return {}
-}
