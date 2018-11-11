8434f83ca949b017882974ab31fe6ec1aefc24c5
Shane Morgan <shane@cd2solutions.co.uk>
2018-11-11 15:16:57 +0000

    asd

--- src/dsl/hasMany.ts
+++
@@ -1,8 +0,0 @@
-import HasManyAssociation from "../HasManyAssociation"
-
-export default function hasMany(target, name, descriptor) {
-  if (!(target instanceof Function)) target = target.constructor
-  const options = descriptor.initializer ? descriptor.initializer() : {}
-  HasManyAssociation.install(target, name, options)
-  return {}
-}
--- src/dsl/hasOne.ts
+++
@@ -1,8 +0,0 @@
-import HasOneAssication from "../HasOneAssociation"
-
-export default function hasOne(target, name, descriptor) {
-  if (!(target instanceof Function)) target = target.constructor
-  const options = descriptor.initializer ? descriptor.initializer() : {}
-  HasOneAssication.install(target, name, options)
-  return {}
-}
